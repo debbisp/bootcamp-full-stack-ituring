be8b4bbe7d6c786d109b2f5d659a1cf7b100ba05
Débora Bispo <deborapbs50@gmail.com>
2022-10-23 00:02:15 -0300

    tipo de variaveis

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -7,8 +7,19 @@ let minhaIdade = 21
 let lugarDoSonho = 'Grécia' 
 
 // exibir o valor das variáveis
+const tipoNome = typeof(meuNome)
+const tipoEmail = typeof(meuEmail)
+const tipoProfissao = typeof(minhaProfissao)
+const tipoIdade = typeof(minhaIdade)
+const tipoLugar = typeof(lugarDoSonho)
+
 console.log (meuNome)
 console.log (meuEmail)
 console.log (minhaProfissao)
 console.log (minhaIdade)
 console.log (lugarDoSonho)
+console.log (tipoNome)
+console.log (tipoEmail)
+console.log (tipoProfissao)
+console.log (tipoIdade)
+console.log (tipoLugar)
