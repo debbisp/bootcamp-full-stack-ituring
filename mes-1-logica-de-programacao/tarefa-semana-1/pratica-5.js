9d6ba5563dd50d7aeeb5aaa896e995c0935028ea
Débora Bispo <deborapbs50@gmail.com>
2022-10-23 01:15:56 -0300

    adicionando textos

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,10 @@
+const meuNome = 'Débora Bispo';
+const meuEmail = 'deborapbs50@gmail.com';
+const minhaProfissao = 'Projetista';
+const minhaIdade = 21;
+const lugarDoSonho = 'Grécia' ;
+
+const mensagemWhats = `Olá, ${meuNome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugarDoSonho}. Confirma pra gente alguns dados? A sua idade é ${minhaIdade}, você é ${minhaProfissao} e o seu email é ${meuEmail}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`;
+
+
+console.log(mensagemWhats);
