5e787e804ef295a69483afa5863b9502558a70d8
Débora Bispo <deborapbs50@gmail.com>
2022-10-23 00:36:53 -0300

    verificar se é verdadeiro ou falso

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,17 @@
+const meuNome = 'Débora Bispo'
+const meuEmail = 'deborapbs50@gmail.com'
+const minhaProfissao = 'Projetista'
+const minhaIdade = 21
+const lugarDoSonho = 'Grécia' 
+
+const ehString = typeof(meuNome) == "string"
+const ehS = typeof(meuEmail) == "string"
+const ehSt = typeof(minhaProfissao) == "string"
+const ehStr = typeof(minhaIdade) == "string"
+const eString = typeof(lugarDoSonho) == "string"
+
+console.log(ehString)
+console.log(ehS)
+console.log(ehSt)
+console.log(ehStr)
+console.log(eString)
