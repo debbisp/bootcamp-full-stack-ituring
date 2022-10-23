47a6afae50b87ab454ecb049c5e14f39aa99cb98
Débora Bispo <deborapbs50@gmail.com>
2022-10-23 01:23:05 -0300

    tabuada

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-6.js
@@ -0,0 +1,35 @@
+//tabuada
+
+let numero1 = 1;
+let numero2 = 2;
+let numero3 = 3;
+let numero4 = 4;
+let numero5 = 5;
+let numero6 = 6;
+let numero7 = 7;
+let numero8 = 8;
+let numero9 = 9;
+let numero10 = 10;
+let num = 123;
+
+let a = numero1 * num;
+let b = numero2 * num;
+let c = numero3 * num;
+let d = numero4 * num;
+let e = numero5 * num;
+let f = numero6 * num;
+let g = numero7 * num;
+let h = numero8 * num;
+let i = numero9 * num;
+let j = numero10 * num;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
