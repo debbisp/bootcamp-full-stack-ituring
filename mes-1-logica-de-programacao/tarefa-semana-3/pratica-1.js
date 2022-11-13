5a8c98c6359c7615e4ba2c903263980592bd2f2b
Débora Bispo <deborapbs50@gmail.com>
2022-11-13 15:56:07 -0300

    adicionando arquivos do mes 2

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-1.js
@@ -0,0 +1,16 @@
+//Prática de número 1 da semana 3
+
+// prática da atividade 1-semana 3
+
+function tabuada (x,n) {
+    let i = 1;
+
+    while (i<=n) {
+        console.log(`${x} x ${i} = ${x*i}`);
+        i++;
+
+    }
+}
+
+tabuada (5,10);
+      
---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-2.js
@@ -0,0 +1,10 @@
+function impar (25) {
+    let i = 1;
+
+    while (i<=impar) {
+        i = i + 2;
+        console.log (i);
+    }
+}
+
+impar (25);
